68d0b2cb0db3021ab8500a8b26ea13821b884362
Vincent <vincentfar7@gmail.com>
2024-04-21 15:18:07 -0400

    restore dotenv and contact env variables

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ ADMIN_EMAIL = os.getenv("EMAIL")
 pw = os.getenv("PASSWORD")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+app.config['SECRET_KEY'] = os.getenv('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -38,7 +38,7 @@ class Base(DeclarativeBase):
 
 
 # CREATE DATABASE
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
